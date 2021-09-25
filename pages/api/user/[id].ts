d3b9e92248ab4f96b9902624e3e16eb967ed87b1
Makapoh54 <makapoh54@gmail.com>
2021-09-25 13:49:35 +0300

    feat: add users [NOT READY]

--- pages/api/user/[id].ts
+++ pages/api/user/[id].ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import fb from "../../utils/firebase";
+import fb from "../../../utils/firebase";
 
 export type User = {
   mindState: string;
