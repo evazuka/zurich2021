84cde4bba67ad554ad971d3122e2765df98761d0
Dmitrijs Minajevs <dmitrijs.minajevs@outlook.com>
2021-09-25 13:54:04 +0300

    Fix

--- pages/api/schedules.ts
+++ pages/api/schedules.ts
@@ -1,16 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
-import fb from "../../utils/firebase";
+import type { NextApiRequest, NextApiResponse } from "next"
+import fb from "../../utils/firebase"
 
 export type Schedule = {
-  userEmail: string;
-  isPersonal: boolean;
-  isGranular: boolean;
-  startTime: string;
-  endTime: string;
-  name: string;
-  socialCircle: string;
-};
+  userEmail: string
+  isPersonal: boolean
+  isGranular: boolean
+  startTime: string
+  endTime: string
+  name: string
+  socialCircle: string
+}
 
 export default async function handler(
   req: NextApiRequest,
@@ -19,12 +19,12 @@ export default async function handler(
   const dataSnapshot = await fb()
     .firestore()
     .collection("schedules")
-    .where("userEmail", "==", req.body.userEmail || 'cheevers@zg.gz')
-    .get();
-  const data: Schedule[] = [];
+    .where("userEmail", "==", req.body.userEmail || "cheevers@zg.gz")
+    .get()
+  const data: Schedule[] = []
   dataSnapshot.forEach((doc: any) => {
-    console.log(doc.id, "=>", doc.data());
-    data.push(doc.data() as Schedule);
-  });
-  res.status(200).json(data);
+    console.log(doc.id, "=>", doc.data())
+    data.push(doc.data() as Schedule)
+  })
+  res.status(200).json(data)
 }
--- utils/firebase.ts
+++ utils/firebase.ts
@@ -1,13 +1,14 @@
-import firebase from "firebase-admin";
-const app = (): any => {
+import firebase from "firebase-admin"
+
+const app = () => {
   if (!firebase.apps.length) {
     return firebase.initializeApp({
       credential: firebase.credential.cert(
         require("./hackzurich2021-firebase-adminsdk-6nrxc-6b545d2df5.json")
       ),
-    });
+    })
   } else {
-    return firebase.app(); // if already initialized, use that one
+    return firebase.app() // if already initialized, use that one
   }
-};
-export default app;
+}
+export default app
