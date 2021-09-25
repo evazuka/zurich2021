2b0ad0274c038ba89495d057a5aa474e178587ba
Dmitrijs Minajevs <dmitrijs.minajevs@outlook.com>
2021-09-25 14:30:51 +0300

    Add LT webhook

--- server.ts
+++ server.ts
@@ -2,14 +2,14 @@ import express from "express"
 import next from "next"
 import path from "path"
 import { config } from "dotenv"
-import { Telegraf } from 'telegraf'
+import { Telegraf } from "telegraf"
 
 const ENV_FILE = path.join(__dirname, ".env")
 config({ path: ENV_FILE })
 
 const token = process.env.BOT_TOKEN
 if (token === undefined) {
-  throw new Error('BOT_TOKEN must be provided!')
+  throw new Error("BOT_TOKEN must be provided!")
 }
 
 const port = parseInt(process.env.PORT ?? "3000", 10)
@@ -18,9 +18,13 @@ const app = next({ dev })
 const handle = app.getRequestHandler()
 
 const bot = new Telegraf(token)
-bot.on('text', (ctx) => ctx.replyWithHTML('<b>Hello</b>'))
+bot.on("text", (ctx) => ctx.replyWithHTML("<b>Hello</b>"))
 
-const secretPath = `/telegraf/${bot.secretPathComponent()}`
+const secretPath = `/bot/${bot.secretPathComponent()}`
+
+if (process.env.DEV) {
+  bot.telegram.setWebhook(`${process.env.LT_PATH}${secretPath}`)
+}
 
 console.log(process.env)
 app.prepare().then(() => {
