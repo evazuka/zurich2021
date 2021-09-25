a922715aa3d82554782d64debd0e8dd0d4383cb6
Makapoh54 <makapoh54@gmail.com>
2021-09-25 15:13:46 +0300

    feat: add schedule creation [NOT FINISHED]

--- pages/api/schedules.ts
+++ pages/api/schedules.ts
@@ -1,30 +1,56 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next"
-import fb from "../../utils/firebase"
+import type { NextApiRequest, NextApiResponse } from "next";
+import fb from "../../utils/firebase";
+import { User } from "./users/[id]";
 
 export type Schedule = {
-  userEmail: string
-  isPersonal: boolean
-  isGranular: boolean
-  startTime: string
-  endTime: string
-  name: string
-  socialCircle: string
-}
+  userId: string;
+  isPersonal: boolean;
+  isGranular: boolean;
+  startTime: string;
+  endTime: string;
+  name: string;
+  socialCircle: string;
+  isPending?: boolean;
+};
+
+export type ScheduleReq = {
+  userId: string;
+  isPersonal: boolean;
+  isGranular: boolean;
+  duration: number;
+  name: string;
+  socialCircle: string;
+};
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Schedule[]>
+  res: NextApiResponse<Schedule[] | any>
 ) {
-  const dataSnapshot = await fb()
-    .firestore()
-    .collection("schedules")
-    .where("userEmail", "==", req.body.userEmail || "cheevers@zg.gz")
-    .get()
-  const data: Schedule[] = []
-  dataSnapshot.forEach((doc: any) => {
-    console.log(doc.id, "=>", doc.data())
-    data.push(doc.data() as Schedule)
-  })
-  res.status(200).json(data)
+  const { body }: { body: ScheduleReq } = req;
+  if (req.method === "GET") {
+    const dataSnapshot = await fb()
+      .firestore()
+      .collection("schedules")
+      .where("userEmail", "==", req.body.userId || "cheevers@zg.gz")
+      .get();
+    const data: Schedule[] = [];
+    dataSnapshot.forEach((doc: any) => {
+      console.log(doc.id, "=>", doc.data());
+      data.push(doc.data() as Schedule);
+    });
+    res.status(200).json(data);
+  } else if (req.method === "POST") {
+    const dataSnapshot = await fb()
+      .firestore()
+      .collection("users")
+      .where("socialCircles", "array-contains-any", [body.socialCircle])
+      .get();
+    const involvedUsers: User[] = [];
+    dataSnapshot.forEach((doc: any) => {
+      console.log(doc.id, "=>", doc.data());
+      involvedUsers.push({...doc.data(), id: doc.id} as User);
+    });
+    res.status(200).json(involvedUsers);
+  }
 }
--- pages/api/users/[id].ts
+++ pages/api/users/[id].ts
@@ -7,6 +7,7 @@ export type User = {
   name: string;
   resilienceRating: number;
   socialCircles: string[];
+  id?: string;
 };
 
 export default async function handler(
