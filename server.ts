e398cb999b66a3de75ce397986c03d4cb8fdeb41
Dmitrijs Minajevs <dmitrijs.minajevs@outlook.com>
2021-09-25 15:11:13 +0300

    Add bot hook

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import express from "express"
 import next from "next"
 import path from "path"
 import { config } from "dotenv"
-import { Telegraf } from "telegraf"
+import { createBot } from "bot"
 
 const ENV_FILE = path.join(__dirname, ".env")
 config({ path: ENV_FILE })
@@ -17,24 +17,21 @@ const dev = process.env.NODE_ENV !== "production"
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
-const bot = new Telegraf(token)
-bot.on("text", (ctx) => ctx.replyWithHTML("<b>Hello</b>"))
-
+const bot = createBot(token)
 const secretPath = `/bot/${bot.secretPathComponent()}`
 
-if (process.env.DEV) {
+if (process.env.DEV == "true") {
   bot.telegram.setWebhook(`${process.env.LT_PATH}${secretPath}`)
 }
 
-console.log(process.env)
 app.prepare().then(() => {
   const server = express()
+
+  server.use(bot.webhookCallback(secretPath))
 
   server.all("*", (req, res) => {
     return handle(req, res)
   })
-
-  server.use(bot.webhookCallback(secretPath))
 
   server.listen(port, () => {
     console.log(`> Ready on http://localhost:${port}`)
