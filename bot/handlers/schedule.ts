2b663eafe4bb047742925f0f5658f6103e84493e
Dmitrijs Minajevs <dmitrijs.minajevs@outlook.com>
2021-09-25 22:25:37 +0300

    convert duration

--- bot/handlers/schedule.ts
+++ bot/handlers/schedule.ts
@@ -3,6 +3,12 @@ import { createScheduleIfPossible } from "pages/api/schedules/[userId]"
 import { Telegraf } from "telegraf"
 import { isNullOrUndefined } from "utils/helpers"
 
+const timeMap = {
+  hour: 60,
+  minutes: 1,
+  seconds: 1 / 60,
+}
+
 export const handleSchedule = (bot: Telegraf<CustomContext>) => {
   bot.command("schedule", async (ctx) => {
     const text = ctx.message.text.replace("/schedule", "").trim()
@@ -26,6 +32,7 @@ export const handleSchedule = (bot: Telegraf<CustomContext>) => {
     if (!ctx.session?.scheduling) return
     const scheduleName = ctx.session.scheduleName
 
+    const text = ctx.message.text
     const match = ctx.message.text.match(/\d+/)
 
     if (isNullOrUndefined(match)) {
@@ -33,16 +40,22 @@ export const handleSchedule = (bot: Telegraf<CustomContext>) => {
       return
     }
 
+    const duration = parseInt(match[0])
+
+    const durationConverted = text.includes("hour")
+      ? timeMap.hour * duration
+      : text.includes("second")
+      ? timeMap.seconds * duration
+      : duration
+
     const chatId = ctx.message.chat.id
     const chat = await bot.telegram.getChat(chatId)
     const isPrivate = chat.type === "private"
 
-    const duration = parseInt(match[0])
-
     const result = await createScheduleIfPossible(ctx.from.id.toString(), {
       isPersonal: isPrivate,
       isGranular: false,
-      duration: duration,
+      duration: durationConverted,
       name: scheduleName,
       socialCircle: isPrivate ? null : (chat as any).title,
     })
