59830a9c34d59f389fea8c118e19ae0634e26b70
Dmitrijs Minajevs <dmitrijs.minajevs@outlook.com>
2021-09-25 12:47:34 +0300

    Log keys on startup

--- server.ts
+++ server.ts
@@ -21,6 +21,8 @@ const dev = process.env.NODE_ENV !== "production"
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
+console.log(process.env)
+
 app.prepare().then(() => {
   const server = express()
 
