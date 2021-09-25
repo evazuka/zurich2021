6f2b1af52b8c0ac17e22f3effc497bce45076e22
evazuka <eva.zhuk@gmail.com>
2021-09-25 14:23:41 +0300

    telegraf added

--- server.ts
+++ server.ts
@@ -2,14 +2,25 @@ import express from "express"
 import next from "next"
 import path from "path"
 import { config } from "dotenv"
+import { Telegraf } from 'telegraf'
 
 const ENV_FILE = path.join(__dirname, ".env")
 config({ path: ENV_FILE })
+
+const token = process.env.BOT_TOKEN
+if (token === undefined) {
+  throw new Error('BOT_TOKEN must be provided!')
+}
 
 const port = parseInt(process.env.PORT ?? "3000", 10)
 const dev = process.env.NODE_ENV !== "production"
 const app = next({ dev })
 const handle = app.getRequestHandler()
+
+const bot = new Telegraf(token)
+bot.on('text', (ctx) => ctx.replyWithHTML('<b>Hello</b>'))
+
+const secretPath = `/telegraf/${bot.secretPathComponent()}`
 
 console.log(process.env)
 app.prepare().then(() => {
@@ -18,6 +29,8 @@ app.prepare().then(() => {
   server.all("*", (req, res) => {
     return handle(req, res)
   })
+
+  server.use(bot.webhookCallback(secretPath))
 
   server.listen(port, () => {
     console.log(`> Ready on http://localhost:${port}`)
