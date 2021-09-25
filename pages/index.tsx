ac642c904acf0f3eb6105ddccef71447a170e267
Makapoh54 <makapoh54@gmail.com>
2021-09-25 14:13:49 +0300

    feat: add user query to ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,17 +4,24 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { Schedule } from "./api/schedules";
+import { User } from "./api/users/[id]";
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 const Home: NextPage = () => {
-  const { data = [], error }: { data?: Schedule[]; error?: any } = useSwr(
+  const { data = [] }: { data?: Schedule[]; error?: any } = useSwr(
     "/api/schedules",
+    fetcher
+  );
+
+  const { data: user }: { data?: User; error?: any } = useSwr(
+    "/api/users/rkHM2tHhjFAjH20jAf9O",
     fetcher
   );
   return (
     <div className={styles.container}>
       {data.map((schedule) => JSON.stringify(schedule))}
+      {JSON.stringify(user)}
     </div>
   );
 };
